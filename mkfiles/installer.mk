3917886758034ea38213e939675668216dd46827
Stefan Wold <ratler@lunar-linux.org>
2012-12-31 10:04:03 +0100

    Copy the whole tree for now

--- mkfiles/installer.mk
+++ mkfiles/installer.mk
@@ -6,7 +6,7 @@ installer: lunar-install
 # Install the Lunar installer
 $(ISO_TARGET)/.lunar-install: iso-target
 	@echo lunar-install
-	@cp $(ISO_SOURCE)/lunar-install/* $(ISO_TARGET)
+	@cp -r $(ISO_SOURCE)/lunar-install/* $(ISO_TARGET)
 	@touch $@
 
 lunar-install: $(ISO_TARGET)/.lunar-install
