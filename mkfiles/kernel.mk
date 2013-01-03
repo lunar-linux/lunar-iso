a042c547b643d00cca6de6ad04b35b5d128d2098
Stefan Wold <ratler@lunar-linux.org>
2013-01-03 12:17:52 +0100

    New generic kernel configs per arch

--- mkfiles/kernel.mk
+++ mkfiles/kernel.mk
@@ -5,7 +5,7 @@ kernel: linux
 
 $(ISO_TARGET)/var/cache/lunar/linux-$(ISO_ARCH).tar.bz2: stage2
 	@echo linux
-	@cp $(ISO_SOURCE)/kernels/conf/generic $(ISO_TARGET)/etc/lunar/local/.config.current
+	@cp $(ISO_SOURCE)/kernels/conf/generic.$(ISO_ARCH) $(ISO_TARGET)/etc/lunar/local/.config.current
 	@yes n | tr -d '\n' | $(ISO_SOURCE)/scripts/chroot-build lin -c linux
 	@mv $(ISO_TARGET)/boot/vmlinuz-* $(ISO_TARGET)/boot/linux
 	@mv $(ISO_TARGET)/boot/initramfs-*.img $(ISO_TARGET)/boot/initrd
