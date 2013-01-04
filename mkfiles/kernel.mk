4193ab8b7b54c734d3c1026b2f54002a42f6e854
Peter de Ridder <peter@lunar-linux.org>
2013-01-04 22:25:48 +0100

    Add kernels sources to the kernel cache file

--- mkfiles/kernel.mk
+++ mkfiles/kernel.mk
@@ -3,12 +3,31 @@
 kernel: linux
 
 
-$(ISO_TARGET)/var/cache/lunar/linux-$(ISO_ARCH).tar.bz2: stage2
+.SECONDARY: $(ISO_TARGET)/var/cache/lunar/linux-$(ISO_ARCH).tar
+$(ISO_TARGET)/var/cache/lunar/linux-$(ISO_ARCH).tar: stage2
 	@echo linux
 	@cp $(ISO_SOURCE)/kernels/conf/generic.$(ISO_ARCH) $(ISO_TARGET)/etc/lunar/local/.config.current
 	@yes n | tr -d '\n' | $(ISO_SOURCE)/scripts/chroot-build lin -c linux
 	@mv $(ISO_TARGET)/boot/vmlinuz-* $(ISO_TARGET)/boot/linux
 	@mv $(ISO_TARGET)/boot/initramfs-*.img $(ISO_TARGET)/boot/initrd
-	@mv $(ISO_TARGET)/var/cache/lunar/linux-$$($(ISO_SOURCE)/scripts/chroot-build lvu installed linux)-$(ISO_BUILD).tar.bz2 $@
+	@bunzip2 -c $(ISO_TARGET)/var/cache/lunar/linux-$$($(ISO_SOURCE)/scripts/chroot-build lvu installed linux)-$(ISO_BUILD).tar.bz2 > $@.tmp
+	@rm $(ISO_TARGET)/var/cache/lunar/linux-$$($(ISO_SOURCE)/scripts/chroot-build lvu installed linux)-$(ISO_BUILD).tar.bz2
+	@mv $@.tmp $@
+
+.INTERMEDIATE: $(ISO_TARGET)/var/cache/lunar/linux-$(ISO_ARCH).files
+$(ISO_TARGET)/var/cache/lunar/linux-$(ISO_ARCH).files: $(ISO_TARGET)/var/cache/lunar/linux-$(ISO_ARCH).tar
+	@find $(ISO_TARGET)/usr/src -path '$(ISO_TARGET)/usr/src/linux*' ! -type d \( \
+	-type l -o \
+	-path '$(ISO_TARGET)/usr/src/linux-*/.config' -o \
+	-path '$(ISO_TARGET)/usr/src/linux-*/Module.symvers' -o \
+	-path '$(ISO_TARGET)/usr/src/linux-*/scripts/*' -o \
+	-path '$(ISO_TARGET)/usr/src/linux-*/include/*' -o \
+	-name 'Makefile*' -o \
+	-name 'Kconfig*' \) -printf 'usr/src/%P\n' > $@
+
+$(ISO_TARGET)/var/cache/lunar/linux-$(ISO_ARCH).tar.bz2: $(ISO_TARGET)/var/cache/lunar/linux-$(ISO_ARCH).tar $(ISO_TARGET)/var/cache/lunar/linux-$(ISO_ARCH).files
+	@tar -rf $< -C $(ISO_TARGET) -T $(ISO_TARGET)/var/cache/lunar/linux-$(ISO_ARCH).files
+	@rm $(ISO_TARGET)/var/cache/lunar/linux-$(ISO_ARCH).files
+	@bzip2 $<
 
 linux: $(ISO_TARGET)/var/cache/lunar/linux-$(ISO_ARCH).tar.bz2
