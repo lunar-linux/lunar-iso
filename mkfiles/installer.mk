a118e2b6c38df5574dfbac719ecbb2166cf6e384
Peter de Ridder <peter@lunar-linux.org>
2013-04-18 21:08:23 +0200

    Updated iso building to match the old installer.

--- mkfiles/installer.mk
+++ mkfiles/installer.mk
@@ -20,4 +20,13 @@ $(ISO_TARGET)/usr/share/lunar-install/locale.list: iso-target
 	  echo -e "$$locale\t$$language ($$territory)" ; \
 	done > $@
 
-lunar-install: $(ISO_TARGET)/.lunar-install $(ISO_TARGET)/usr/share/lunar-install/locale.list
+$(ISO_TARGET)/usr/share/lunar-install/moonbase.tar.bz2: $(ISO_SOURCE)/spool/moonbase.tar.bz2 iso-target
+	@cp $< $@
+
+$(ISO_TARGET)/README: $(ISO_SOURCE)/template/README iso-target
+	@sed -e 's:%VERSION%:$(ISO_VERSION):g' -e 's:%CODENAME%:$(ISO_CODENAME):g' -e 's:%DATE%:$(ISO_DATE):g' -e 's:%KERNEL%:$(ISO_KERNEL):g' -e 's:%CNAME%:$(ISO_CNAME):g' -e 's:%COPYRIGHTYEAR%:$(ISO_COPYRIGHTYEAR):g' -e 's:%LABEL%:$(ISO_LABEL):' $< > $@
+
+$(ISO_TARGET)/usr/share/lunar-install/motd: $(ISO_SOURCE)/template/motd iso-target
+	@sed -e 's:%VERSION%:$(ISO_VERSION):g' -e 's:%CODENAME%:$(ISO_CODENAME):g' -e 's:%DATE%:$(ISO_DATE):g' -e 's:%KERNEL%:$(ISO_KERNEL):g' -e 's:%CNAME%:$(ISO_CNAME):g' -e 's:%COPYRIGHTYEAR%:$(ISO_COPYRIGHTYEAR):g' -e 's:%LABEL%:$(ISO_LABEL):' $< > $@
+
+lunar-install: $(ISO_TARGET)/.lunar-install $(ISO_TARGET)/usr/share/lunar-install/locale.list $(ISO_TARGET)/usr/share/lunar-install/moonbase.tar.bz2 $(ISO_TARGET)/README $(ISO_TARGET)/usr/share/lunar-install/motd
