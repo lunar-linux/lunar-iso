345edaf3e0add3a47c7c7ca1f92053d3cfafd1cb
Dave Brown <dagbrown@lart.ca>
2017-08-08 07:09:20 +0000

    Add alias for %OSSL.
    When building an ISO, we don't care about offering a choice: that can
    happen later on, when the user has installed the system.
    
    I picked openssl arbitarily.

--- mkfiles/stage1.mk
+++ mkfiles/stage1.mk
@@ -43,6 +43,7 @@ include $(ISO_SOURCE)/conf/modules.stage1
 
 $(ISO_TARGET)/.stage1: stage1-toolchain
 	@echo stage1-build
+	@grep LUNAR_ALIAS_OSSL BUILD/etc/lunar/local/config || echo 'LUNAR_ALIAS_OSSL="openssl"' >> BUILD/etc/lunar/local/config
 	@yes n | tr -d '\n' | $(ISO_SOURCE)/scripts/chroot-build lin -rc $(filter-out $(TOOLCHAIN_MODULES),$(STAGE1_MODULES))
 	@touch $@
 
