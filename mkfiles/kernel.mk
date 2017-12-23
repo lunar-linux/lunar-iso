14708d5f2f18bc4a58037d8b4837fd706bcedf00
Dave Brown <dagbrown@lart.ca>
2017-12-23 15:15:33 +0900

    Add zfs to the kernel build, through trickery.
    Do it by an evil cheat because you can't build SPL or ZFS without the
    kernel being present, but the kernel's POST_INSTALL can take care of
    that automatically if it thinks it's installed.
    
    Fiddling with accepted licenses is also pretty evil.

--- mkfiles/kernel.mk
+++ mkfiles/kernel.mk
@@ -2,10 +2,14 @@
 
 kernel: linux
 
-
 .SECONDARY: $(ISO_TARGET)/var/cache/lunar/linux-$(ISO_ARCH).tar
 $(ISO_TARGET)/var/cache/lunar/linux-$(ISO_ARCH).tar: stage2
 	@echo linux
+	# cheat it into thinking zfs is installed so that the kernel
+	# POST_INSTALL takes care of that
+	@echo "zfs:$(date +%Y%m%d):installed:x:x" >> $(ISO_TARGET)/var/state/lunar/packages
+	# This is naughtier. Fiddle with accepted licenses to allow zfs to build.
+	@echo 'ACCEPTED_LICENSES="cddl"' >> $(ISO_TARGET)/etc/lunar/local/config
 	@cp $(ISO_SOURCE)/kernels/conf/generic.$(ISO_ARCH) $(ISO_TARGET)/etc/lunar/local/.config.current
 	@for mod in $(KERNEL_MODULES); do \
 		yes n | tr -d '\n' | $(ISO_SOURCE)/scripts/chroot-build lin -c $$mod; \
