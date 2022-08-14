7fd1151b21b7e5369d77f527a1eee2c214884c2f
Stefan Wold <ratler@lunar-linux.org>
2022-08-14 11:53:58 +0200

    Fix remnants after previous locale.list change

--- mkfiles/installer.mk
+++ mkfiles/installer.mk
@@ -8,7 +8,7 @@ $(ISO_TARGET)/sbin/lunar-install:
 	@make -C lunar-install install DESTDIR=$(ISO_TARGET)
 
 $(ISO_TARGET)/usr/share/lunar-install/moonbase.tar.bz2: $(ISO_SOURCE)/spool/moonbase.tar.bz2 iso-target
-	@cp $< $@
+	@install -Dm644 $< $@
 
 $(ISO_TARGET)/README: $(ISO_SOURCE)/template/README iso-target
 	@sed -e 's:%VERSION%:$(ISO_VERSION):g' -e 's:%CODENAME%:$(ISO_CODENAME):g' -e 's:%DATE%:$(ISO_DATE):g' -e 's:%KERNEL%:$(ISO_KERNEL):g' -e 's:%CNAME%:$(ISO_CNAME):g' -e 's:%COPYRIGHTYEAR%:$(ISO_COPYRIGHTYEAR):g' -e 's:%LABEL%:LUNAR_$(ISO_MAJOR):' $< > $@
@@ -17,7 +17,6 @@ $(ISO_TARGET)/usr/share/lunar-install/motd: $(ISO_SOURCE)/template/motd iso-targ
 	@sed -e 's:%VERSION%:$(ISO_VERSION):g' -e 's:%CODENAME%:$(ISO_CODENAME):g' -e 's:%DATE%:$(ISO_DATE):g' -e 's:%KERNEL%:$(ISO_KERNEL):g' -e 's:%CNAME%:$(ISO_CNAME):g' -e 's:%COPYRIGHTYEAR%:$(ISO_COPYRIGHTYEAR):g' -e 's:%LABEL%:LUNAR_$(ISO_MAJOR):' $< > $@
 
 lunar-install: $(ISO_TARGET)/sbin/lunar-install \
-	$(ISO_TARGET)/usr/share/lunar-install/locale.list \
 	$(ISO_TARGET)/usr/share/lunar-install/moonbase.tar.bz2 \
 	$(ISO_TARGET)/README \
 	$(ISO_TARGET)/usr/share/lunar-install/motd
